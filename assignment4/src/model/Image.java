7f82674227f6d87b8c9856c012458fc673cf8144
Dheeraj <dheerajreddy.in@gmail.com>
2024-10-19 20:50:21 -0400

    updated the Image class as discussed

--- assignment4/src/model/Image.java
+++ assignment4/src/model/Image.java
@@ -14,7 +14,15 @@ public class Image {
   // isEmpty tracks whether an Image is instantiated as an empty object or with parameters.
   private boolean isEmpty = false;
 
+
   // @todo add private final members as required.
+  private int height;
+  private int width;
+  private int noOfChannels;
+  private int[][] redChannelData;
+  private int[][] greenChannelData;
+  private int[][] blueChannelData;
+  private int[][] transparency;
 
   /**
    * This constructor is used to instantiate an "empty" Image object.
@@ -25,10 +33,46 @@ public class Image {
   }
 
   public Image(int[][][] image) {
-    int height = image.length;
-    int width = image[0].length;
-    int num_channels = image[0][0].length;
-    // @todo Complete implementation here. int[][][] has [width][height][num_channels]
+    height = arr.length;
+    width = arr[0].length;
+    noOfChannels = arr[0][0].length;
+
+
+    redChannelData = new int[height][width];
+    greenChannelData = new int[height][width];
+    blueChannelData = new int[height][width];
+    value = new int[height][width];
+    intensity = new int[height][width];
+    luma = new int[height][width];
+
+    if (noOfChannels==4){
+      transparency = new int[height][height];
+    }
+
+
+    for(int i=0; i< height ; i++){
+      for(int j=0; j<width; j++){
+        redChannelData[i][j] = arr [i][j][0];
+        greenChannelData[i][j] = arr[i][j][1];
+        blueChannelData[i][j] = arr[i][j][2];
+        if(noOfChannels == 4){
+          transparency[i][j] = arr[i][j][2];
+
+        }
+      }
+    }
+  }
+
+  public int getHeight(){
+    return height;
+  }
+
+  public int getWidth(){
+    return width;
+  }
+
+  public int getNoOfChannels(){
+    return noOfChannels;
   }
 
   /**
@@ -40,11 +84,69 @@ public class Image {
     return isEmpty;
   }
 
-
-  public int[][][] getImageArray() {
-    throw new UnsupportedOperationException("Not supported yet.");
-    // @todo Need to convert to 3d array [width][height][num_channels]
+  public int[][] getRedChannelData(){
+    return redChannelData;
   }
+
+  public int[][] getGreenChannelData(){
+    return greenChannelData;
+  }
+
+  public int[][] getBlueChannelData(){
+    return blueChannelData;
+  }
+
+  public int[][] getTransparencyData(){
+    return transparency;
+  }
+
+  public int[] getPixelData(int i, int j){
+    if(noOfChannels == 3){
+      int[] arr = new int[3];
+      arr[0] = redChannelData[i][j];
+      arr[1] = greenChannelData[i][j];
+      arr[2] = blueChannelData[i][j];
+    }
+    else if(noOfChannels == 4){
+      int[] arr = new int[4];
+      arr[0] = redChannelData[i][j];
+      arr[1] = greenChannelData[i][j];
+      arr[2] = blueChannelData[i][j];
+      arr[3] = transparency[i][j];
+    }
+  }
+
+  public int[][][] getImageArray(){
+    int imageArr[][][] = new int[this.height][this.width][this.noOfChannels];
+    for(int i=0; i<this.height ; i++){
+      for(int h=0; j<this.width; j++){
+        imageArr[i][j][0] = redChannelData[i][j];
+      }
+    }
+
+    for(int i=0; i<this.height ; i++){
+      for(int h=0; j<this.width; j++){
+        imageArr[i][j][1] = greenChannelData[i][j];
+      }
+    }
+
+    for(int i=0; i<this.height ; i++){
+      for(int h=0; j<this.width; j++){
+        imageArr[i][j][2] = blueChannelData[i][j];
+      }
+    }
+
+    if(noOfChannels == 4){
+      for(int i=0; i<this.height ; i++){
+        for(int h=0; j<this.width; j++){
+          imageArr[i][j][3] = transparency[i][j];
+        }
+      }
+    }
+
+    return imageArr;
+  }
+
 
   /**
    * This class represents a temporary storage medium for a collection of {@code model.Image}. A
