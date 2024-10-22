1eca471ad1dae95095cc83e4507a58fec3991d58
Surender Soundiramourty <psm.surender@gmail.com>
2024-10-22 19:04:21 -0400

    Create App.java.

---
+++ assignment4/src/app/App.java
@@ -0,0 +1,13 @@
+package app;
+
+import controller.Controller;
+import model.Model;
+
+public class App {
+
+  public static void main(String[] args) {
+    Controller controller = new Controller(System.in, System.out);
+    controller.go(new Model());
+  }
+
+}
