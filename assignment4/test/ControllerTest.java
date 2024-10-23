4447b62f0e0aceeb1d19fd3f6af11bdd6bd12782
Surender Soundiramourty <psm.surender@gmail.com>
2024-10-23 09:16:21 -0400

    Wrote some script tests

---
+++ assignment4/test/ControllerTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+public class ControllerTest {
+
+  @Test
+  public void loadExistingFile() {}
+
+  @Test
+  public void loadNonExistingFile() {}
+
+  @Test
+  public void saveExistingImageToFile() {}
+
+  @Test
+  public void saveNonExistingImageToFile() {}
+
+}
