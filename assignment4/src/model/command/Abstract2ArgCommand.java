d763e5dd8f1bf4349586517503739cb9921d15df
Surender Soundiramourty <psm.surender@gmail.com>
2024-11-01 15:02:22 -0400

    Abstract classes for 2 and 3 arguments

---
+++ assignment4/src/model/command/Abstract2ArgCommand.java
@@ -0,0 +1,19 @@
+package model.command;
+
+import model.Image;
+
+/**
+ * An abstract class for all commands with 2 arguments where first argument is an Image and the
+ * second is the resulting image name.
+ */
+abstract class Abstract2ArgCommand extends AbstractCommand {
+
+  Abstract2ArgCommand(String rawArguments) {
+    super(rawArguments);
+    if (numberOfArgs() != 2) {
+      throw new IllegalArgumentException("Expected 2 arguments.");
+    }
+    currentImage = Image.Cache.get(getArg(0));
+    imageName = getArg(1);
+  }
+}
