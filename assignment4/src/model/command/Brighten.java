b3daaf60279819128175dca64c4204143cdecbf1
Surender Soundiramourty <psm.surender@gmail.com>
2024-11-03 14:46:34 -0500

    Refactored Brighten

--- assignment4/src/model/command/Brighten.java
+++ assignment4/src/model/command/Brighten.java
@@ -8,7 +8,6 @@ import model.Cache;
  */
 class Brighten extends AbstractCommand {
 
-  private final ImageProcessor processor;
   private final int value;
 
   /**
@@ -28,38 +27,17 @@ class Brighten extends AbstractCommand {
     }
     currentImage = cache.get(getArg(1));
     imageName = getArg(2);
-    // set up processor to brighten image.
-    processor = new ImageProcessor(currentImage, imageName, cache) {
-      @Override
-      public void execute() {
-        processImage(createBrightenTransformer());
-      }
-    };
-  }
-
-
-  private PixelTransformer createBrightenTransformer() {
-    return (r, g, b) -> {
-      int[] result = new int[3];
-
-      // Process red channel
-      result[0] = r != 0 ? ImageProcessor.clamp(r + value) : 0;
-
-      // Process other channels based on number of channels
-      if (currentImage.getNoOfChannels() > 1) {
-        result[1] = g != 0 ? ImageProcessor.clamp(g + value) : 0;
-      }
-
-      if (currentImage.getNoOfChannels() > 2) {
-        result[2] = b != 0 ? ImageProcessor.clamp(b + value) : 0;
-      }
-
-      return result;
-    };
   }
 
   @Override
   public void execute() {
-    processor.execute();
+    new ImageProcessor(currentImage, imageName, cache) {
+      @Override
+      public void execute() {
+        processImage((r, g, b) -> {
+          return new int[]{clamp(r + value), clamp(g + value), clamp(b + value)};
+        });
+      }
+    }.execute();
   }
 }
