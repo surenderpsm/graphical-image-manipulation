b7c43b3076181bbd4804665260090cdb358a867c
Surender Soundiramourty <psm.surender@gmail.com>
2024-11-06 13:33:10 -0500

    removed debug statements

--- assignment4/src/model/command/ColorCorrection.java
+++ assignment4/src/model/command/ColorCorrection.java
@@ -16,12 +16,7 @@ class ColorCorrection extends SimpleImageProcessor {
     super(rawArguments, cache);
     histogram = new Histogram(currentImage);
     setChannelMax();
-    System.out.println("this.peakPositions = " + Arrays.toString(this.peakPositions));
     setAverage();
-    System.out.println("average = " + average);
-    System.out.println("getChannelOffset(0) = " + getChannelOffset(0));
-    System.out.println("getChannelOffset(1) = " + getChannelOffset(1));
-    System.out.println("getChannelOffset(2) = " + getChannelOffset(2));
     setTransformer((r,g,b)-> new int[]{
         clamp(r + getChannelOffset(0)),
         clamp(g + getChannelOffset(1)),
@@ -35,10 +30,7 @@ class ColorCorrection extends SimpleImageProcessor {
       int peakFrequency = 0;
       int maxValue = 0;
       for (int i = 10; i < 245; i++) {
-        System.out.println("i = " + i);
-        System.out.println("histogram.getChannelFrequencyValue(channel, i) = " + histogram.getChannelFrequencyValue(channel, i));
         if (peakFrequency < histogram.getChannelFrequencyValue(channel, i)) {
-          System.out.println("changgin value");
           peakFrequency = histogram.getChannelFrequencyValue(channel, i);
           maxValue = i;
         }
