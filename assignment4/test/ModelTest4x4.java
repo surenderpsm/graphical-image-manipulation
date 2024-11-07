b7b1d6120b1c87a05b636328e56643833eeee620
Surender Soundiramourty <psm.surender@gmail.com>
2024-11-07 09:43:26 -0500

    Tests added by @dheeraj-creddy

--- assignment4/test/ModelTest4x4.java
+++ assignment4/test/ModelTest4x4.java
@@ -2,6 +2,8 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 /**
  * Tests for the model on a 4x4 matrix image stored as an array in res/img4x4/original.txt. Expected
  * outputs are also stored as arrays in text files.
@@ -35,6 +37,102 @@ public class ModelTest4x4 extends AbstractModelTest {
     model.execute("sharpen", "horizontal horizontal-Sharpen");
     model.execute("brighten", "100 horizontal-Sharpen horizontal-Sharpen-brighten");
     assertTrue(isEqual(expected, model.getImage("horizontal-Sharpen-brighten")));
+  }
+
+  /**
+   * test for compression.
+   */
+  @Test
+  public void compressionTest() {
+    int[][][] expected = get3DArrayFromFile(getRoot() + "compression.txt");
+    model.execute("compress", "60 image compressed");
+    int[][][] arr = model.getImage("compressed");
+    System.out.println(Arrays.deepToString(arr));
+    assertTrue(isEqual(expected, arr));
+  }
+
+  /**
+   * test for invalid input compression.
+   */
+  @Test (expected = IllegalArgumentException.class)
+  public void compressionTest2() {
+    int[][][] expected = get3DArrayFromFile(getRoot() + "compression.txt");
+    model.execute("compress", "0.60 image compressed");
+  }
+
+  /**
+   * test for histogram.
+   */
+  @Test
+  public void histogramTest() {
+    int[][] expected = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1
+        , 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0
+        , 1, 0, 0, 0, 0, 0, 2, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0
+        , 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    }, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        , 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0
+        , 0, 1, 1, 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0
+        , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0
+        , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        , 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        , 0, 0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 0, 1, 0, 0, 2, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1
+        , 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        , 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        , 0, 0, 0, 0, 0, 0, 0}};
+    model.execute("histogram", "image histogram");
+    int[][] arr = model.getHistogram("histogram");
+    System.out.println(Arrays.deepToString(arr));
+    assertTrue(isEqual2D(expected, arr));
+  }
+
+  /**
+   * test for color correction.
+   */
+  @Test
+  public void ColorCorrectionTest() {
+    int[][][] expected = {{{142, 119, 122}, {147, 133, 140}, {155, 152, 153}, {113, 112, 126}},
+        {{135, 111, 119}, {119, 119, 141}, {134, 158, 170}, {120, 131, 144}},
+        {{109, 104, 129}, {89, 93, 129}, {123, 113, 133}, {143, 163, 156}},
+        {{88, 103, 131}, {83, 82, 119}, {153, 149, 150}, {119, 128, 135}}};
+
+    model.execute("color-correct", "image colorCorrected");
+    int[][][] arr = model.getImage("colorCorrected");
+    System.out.println(Arrays.deepToString(arr));
+    assertTrue(isEqual(expected, arr));
+  }
+
+  /**
+   * test for levels adjust.
+   */
+  @Test
+  public void levelsAdjustTest() {
+    int[][][] expected  =  {
+        {{199, 95, 90}, {204, 109, 108}, {212, 128, 121}, {170, 88, 94}},
+        {{192, 87, 87}, {176, 95, 109}, {191, 134, 138}, {177, 107, 112}},
+        {{166, 80, 97}, {146, 69, 97}, {180, 89, 101}, {200, 139, 124}},
+        {{145, 79, 99}, {140, 58, 87}, {210, 125, 118}, {176, 104, 103}}
+    };
+
+    model.execute("levels-adjust", "0 128 255 image levelAdjusted");
+    // levels-adjust b m w image-name dest-image-name"
+    int[][][] arr = model.getImage("levelAdjusted");
+    System.out.println(Arrays.deepToString(arr));
+    System.out.println(Arrays.deepToString(expected));
+    assertTrue(isEqual(expected, arr));
   }
 
 }
