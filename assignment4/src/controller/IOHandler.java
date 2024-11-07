e29531d7ca3228a9d0e9ed46da60ee972acfd045
Surender Soundiramourty <psm.surender@gmail.com>
2024-11-07 11:59:13 -0500

    Refactored IOHandler.
    support to create  parent directory if they don't exist.

--- assignment4/src/controller/IOHandler.java
+++ assignment4/src/controller/IOHandler.java
@@ -2,6 +2,8 @@ package controller;
 
 import controller.imagehandler.ImageHandler;
 import controller.imagehandler.ImageHandlerSelector;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import model.Model;
 
@@ -9,22 +11,51 @@ import model.Model;
  * The {@code IOHandler} class is responsible for executing  out IO related commands.
  */
 public class IOHandler {
-
+  Model model;
+  String name;
+  String path;
+  ImageHandler ih;
+  String command;
   public IOHandler(Model model, String command, String args) throws IOException {
+    this.model = model;
+    this.command = command;
+
     if (!command.equals("load") && !command.equals("save")) {
       throw new IllegalArgumentException("Invalid command. Supported IO commands are: load, save");
     }
 
     String[] argsArray = parseArgs(args);
-    String path = argsArray[0];
-    String name = argsArray[1];
+    path = argsArray[0];
+    name = argsArray[1];
 
-    ImageHandler ih = findMatchingImageHandler(path);
+    createParentDirectories();
+
+    ih = findMatchingImageHandler(path);
     if (ih == null) {
       throw new IllegalArgumentException(
           "No matching ImageHandler found for the provided arguments.");
     }
 
+    commandSelector();
+  }
+
+  private void createParentDirectories() throws IOException {
+    // Create a File object for the path
+    File file = new File(path);
+
+    // Get the parent directory of the file
+    File parentDir = file.getParentFile();
+
+    // Check if the parent directory exists; if not, create it
+    if (parentDir != null && !parentDir.exists()) {
+      // Creates the directory including any necessary but nonexistent parent directories
+      if(!parentDir.mkdirs()){
+        // directory creation failed.
+        throw new IOException("Couldn't create parent directory "+parentDir);
+      }
+    }
+  }
+  private void commandSelector() throws IOException {
     switch (command) {
       case "load":
         model.setImage(ih.loadImage(), name);
@@ -34,14 +65,16 @@ public class IOHandler {
           ih.saveImage(new HistogramGenerator(model.getHistogram(name)).getImage());
         }
         else {
-          ih.saveImage(model.getImage(name));
+          try {
+            ih.saveImage(model.getImage(name));
+          } catch (FileNotFoundException e) {
+            throw new IllegalArgumentException(e.getMessage());
+          }
         }
         break;
       default:
         throw new UnsupportedOperationException("Unsupported command: " + command);
     }
-
-
   }
 
   // Helper method to parse arguments
