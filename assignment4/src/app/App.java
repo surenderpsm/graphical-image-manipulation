f0a1bfa63eee928dab0b4a0129943921a96ec8e7
Surender Soundiramourty <psm.surender@gmail.com>
2024-11-09 14:48:16 -0500

    Added gui entry point.
    no cli args for gui, -text for cli, and -file for script mode.

--- assignment4/src/app/App.java
+++ assignment4/src/app/App.java
@@ -4,7 +4,9 @@ import controller.Controller;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import javax.swing.SwingUtilities;
 import model.Model;
+import view.DefaultFrame;
 
 /**
  * Represents our applications main method containing class.
@@ -13,31 +15,30 @@ public class App {
 
   /**
    * This is the main method.
+   *
    * @param args cli args
    */
   public static void main(String[] args) {
-    String filePath = null;
     for (int i = 0; i < args.length; i++) {
       if (args[i].equals("-file") && i + 1 < args.length) {
-        filePath = args[i + 1];
-        break;
+        String filePath = args[i + 1];
+        // If a file path is specified, use it as input
+        System.out.println("Loading file: " + filePath);
+        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
+          new Controller(fileInputStream, System.out).run(new Model());
+        } catch (FileNotFoundException e) {
+          System.out.println("File not found: " + filePath);
+        } catch (IOException e) {
+          System.out.println("Error reading file: " + filePath);
+        }
+        return;
+      }
+      if (args[i].equals("-text")) {
+        new Controller(System.in, System.out).run(new Model());
+        return;
       }
     }
-    if (filePath != null) {
-      // If a file path is specified, use it as input
-      System.out.println("Loading file: " + filePath);
-      try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
-        new Controller(fileInputStream, System.out).run(new Model());
-      } catch (FileNotFoundException e) {
-        System.out.println("File not found: " + filePath);
-      } catch (IOException e) {
-        System.out.println("Error reading file: " + filePath);
-      }
-    }
-    else {
-      // If no file path is specified, use System.in as input
-      new Controller(System.in, System.out).run(new Model());
-    }
+    SwingUtilities.invokeLater(DefaultFrame::new);
   }
 
 }
